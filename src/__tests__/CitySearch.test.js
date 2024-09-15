67a8a74610af266b24c46e189d76b0437dc1fdd6
Monica Alyssa <monica_alyssa@icloud.com>
2024-09-14 21:39:10 -0400

    Update CitySearch test and component for rerendering

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import CitySearch from "../components/CitySearch";
 import userEvent from "@testing-library/user-event";
+import { extractLocations, getEvents } from "../api";
 
 describe('<CitySearch /> component', () => {
     let CitySearchComponent;
@@ -27,4 +28,24 @@ describe('<CitySearch /> component', () => {
         expect(suggestionList).toBeInTheDocument();
         expect(suggestionList).toHaveClass('suggestions');
     })
+
+    test('Updates list of suggestions correctly when a user types in city textbox', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, "Berlin");
+
+        const suggestions = allLocations? allLocations.filter((location) => {
+            return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
+        }): [];
+
+        const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
+        expect(suggestionListItems).toHaveLength(suggestions.length + 1);
+        for (let i = 0; i < suggestions.length; i +=1) {
+            expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
+        }
+    });
 });
