d46b194a8efa34140e84d95647b16071bd04098a
Monica Alyssa <monica_alyssa@icloud.com>
2024-09-14 21:57:14 -0400

    Update CitySearch test for selection from the suggestion list

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -48,4 +48,19 @@ describe('<CitySearch /> component', () => {
             expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
         }
     });
+
+    test('Renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, "Berlin");
+
+        const BerlinGermanySuggestion = CitySearchComponent.queryAllByRole('listitem')[0];
+
+        await user.click(BerlinGermanySuggestion);
+        expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+    });
 });
