727075c95f554aa273afe8956bce0feafe215de3
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-01 14:17:21 -0400

    Add filter get the total number of events per city

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -1,8 +1,21 @@
 import { useState, useEffect } from "react";
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
-const CityEventsChart = () => {
+const CityEventsChart = ({ allLocations, events }) => {
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        setData(getData());
+    }, [`${events}`]);
+
+    const getData = () => {
+        const data = allLocations.map((location) => {
+            const count = events.filter((event) => event.location === location).length
+            const city = location.split(', ')[0]
+            return { city, count };
+        })
+        return data;
+    };
 }
 
-export default cityEventsChart;
+export default CityEventsChart;
