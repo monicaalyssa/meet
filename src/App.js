4abd2da6c1a639b14cbc0ddec62cb039b67ebddb
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-01 13:30:22 -0400

    Add warning alert subclass

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { getEvents, extractLocations } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alerts';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alerts';
 import './App.css';
 
 function App() {
@@ -13,8 +13,14 @@ function App() {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    if (!navigator.onLine) {
+      setWarningAlert("You are offline. Events data may be outdated.");
+    } else {
+      setWarningAlert("")
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -34,6 +40,7 @@ function App() {
       <div className='alerts-container'>
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
       </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/>
       <NumberOfEvents setCurrentNOE={setCurrentNOE} onChangeNOE={handleNOEChange} setErrorAlert={setErrorAlert} />
--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -48,5 +48,13 @@ class ErrorAlert extends Alert {
     }
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'rgb(255, 0, 0)';
+        this.bgColor = 'rgb(244, 180, 180)';
+    }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
 
