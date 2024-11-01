278a682715a6f05b1c18f371596fe52b0529420d
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-01 14:01:47 -0400

    Add city events scatter chart component

---
+++ src/components/CityEventsChart.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+
+const cityEventsChart = () => {
+
+}
+
+export default cityEventsChart;
