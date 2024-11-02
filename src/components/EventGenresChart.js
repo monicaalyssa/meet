21904db64e24a8e67ab61a5d6a1c061ddbb327ad
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-02 12:42:20 -0400

    Update pie chart to filter event genre data

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -2,7 +2,20 @@ import { useState, useEffect } from "react";
 import { ResponsiveContainer, PieChart, Pie, Legend } from 'recharts';
 
 const EventGenresChart = ({ events }) => {
+    const [data, setData] = useState([]);
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
 
-}
+    useEffect(() => {
+        setData(getData());
+    }, [`${events}`]);
+
+    const getData = () => {
+        const data = genres.map(genre => {
+          const filteredEvents = events.filter(event => event.summary.includes(genre));  
+          return { name: genre, value: filteredEvents.length }
+        })
+        return data;
+    };
+};
 
 export default EventGenresChart;
