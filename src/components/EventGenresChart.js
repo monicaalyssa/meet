19c397419a69cb02ac6fe90a9a7954d93c31c121
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-02 12:43:45 -0400

    Update pie chart component to display chart

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -16,6 +16,21 @@ const EventGenresChart = ({ events }) => {
         })
         return data;
     };
+
+    return (
+        <ResponsiveContainer width="99%" height={400}>
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              fill="#8884d8"
+              labelLine={false}
+              label
+              outerRadius={130}           
+            />
+          </PieChart>
+        </ResponsiveContainer>
+      );
 };
 
 export default EventGenresChart;
